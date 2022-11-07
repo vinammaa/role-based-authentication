c429c0a700eb15114f01449550ebd45144c94a12
vinammaa <v.pandranki@finmont.com>
2022-11-07 12:27:49 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const data = [
+  {
+    id: 1,
+    name: "vineela",
+    isAdmin: true,
+    password: "realname"
+  },
+   {
+    id: 2,
+    name: "vinamma",
+    isAdmin: false,
+    password: "petname"
+  },
+   {
+    id: 1,
+    name: "viny",
+    isAdmin: false,
+    password: "nickname"
+  }
+];
+
+
+const SignIn = () => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [admin, setAdmin] = useState(false);
+  
+  const authenicateUser =  (e) => {
+  e.preventDefault();
+    data.map(item => {
+      
+      if (name === item.name && password === item.password) {
+        if (item.isAdmin) {
+          setAdmin(item.isAdmin);
+        }
+      }
+    })
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  < >
+  {
+    !admin ?
+  <form onSubmit={authenicateUser}>
+      <input onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter Name" />
+      <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter Password" />
+      <button type="submit">Login</button>
+    </form> :
+    <h1>Welcome, {name}</h1>
+  } 
+  </>
+  )
 
-export default App;
+}
+export default SignIn;
